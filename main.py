46646b7e6ac47e6df8d8bd86300f1875a17045e2
hansung <gnidoc327@gmail.com>
2022-04-14 12:03:04 +0900

    Add lambda

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+import json
+import boto3
 from flask import Flask
 
 app = Flask(__name__)
+lambda_client = boto3.client('lambda', region_name='ap-northeast-2')
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/invoke")
+def invoke():
+    res = lambda_client(
+        FunctionName='invoke_lambda',
+        Payload=json.dumps({
+            'hostname': 'example.com',
+            'port': 443,
+            'path': '/',
+            'method': 'GET'
+        }))
+    return res
